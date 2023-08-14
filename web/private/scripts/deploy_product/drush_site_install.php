11c0b4e013bb7e0955c4374e07854a1e27374391
Kevin Wall <kevin@kwallcompany.com>
2023-08-13 17:20:38 -0700

    kick off scripts.

--- web/private/scripts/deploy_product/drush_site_install.php
+++ web/private/scripts/deploy_product/drush_site_install.php
@@ -1,5 +1,5 @@
 <?php
 
 print "Installing Drupal...\n";
-passthru('drush site:install --site-name="KWALL Drupal Starter" --site-mail="info@kwallcompany.com" -y');
+passthru('drush site:install --site-name="KWALL Drupal 10 Starter" --site-mail="info@kwallcompany.com" -y');
 print "Install Drupal complete.\n";
